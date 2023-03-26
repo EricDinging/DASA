ff7de8de7391a7d27f03996a01029f778d253d59
EricDinging <ericdingg16@outlook.com>
2023-03-25 23:02:22 -0400

    upd layout

---
+++ central_control/Core/Inc/central_control.h
@@ -0,0 +1,13 @@
+/*
+ * central_control.h
+ *
+ *  Created on: Mar 25, 2023
+ *      Author: eric
+ */
+
+#ifndef INC_CENTRAL_CONTROL_H_
+#define INC_CENTRAL_CONTROL_H_
+
+
+
+#endif /* INC_CENTRAL_CONTROL_H_ */
---
+++ central_control/Core/Src/central_control.c
@@ -0,0 +1,8 @@
+/*
+ * central_control.c
+ *
+ *  Created on: Mar 25, 2023
+ *      Author: eric
+ */
+
+
